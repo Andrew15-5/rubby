574b513758cbff882118c29a0aad2c3abd2f4156
Andrew Voynov <andrewvoynov.b@gmail.com>
2023-09-14 20:53:40 +0300

    Fixed typos in ./scripts/generate-notes.sh

--- scripts/generate-notes.sh
+++ scripts/generate-notes.sh
@@ -3,18 +3,18 @@ cd "$(dirname "$0")" || exit 1
 version=$(sh ./get-version.sh "$@")
 
 changelog_file='../CHANGELOG.md'
-regex_tag=$(echo "$version" | sed 's/\./\\&/g')
+regex_version=$(echo "$version" | sed 's/\./\\&/g')
 
-specific_version_begin_patter='/## \['"$regex_tag"']/'
-version_begin_patter='/## \[[0-9]+\.[0-9]+\.[0-9]+\]/'
+specific_version_begin_pattern='/## \['"$regex_version"']/'
+version_begin_pattern='/## \[[0-9]+\.[0-9]+\.[0-9]+\]/'
 
-sed_command1="${specific_version_begin_patter}="
+sed_command1="${specific_version_begin_pattern}="
 
 version_begin_line_number=$(sed -n -E "$sed_command1" "$changelog_file")
 plus_one_line=$((version_begin_line_number + 1))
 
 sed_command2="1,${plus_one_line}d"
-sed_command3="${version_begin_patter}="
+sed_command3="${version_begin_pattern}="
 
 version_begin_line_number=$(
   sed -n -E "$sed_command2;$sed_command3" "$changelog_file"
